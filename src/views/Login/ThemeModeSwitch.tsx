09162a30120486671b5f54fe502221b81ff5a569
Microler <414464322@qq.com>
2022-11-05 14:24:48 +0800

    feat: improve modeswitch button

--- src/views/Login/ThemeModeSwitch.tsx
+++ src/views/Login/ThemeModeSwitch.tsx
@@ -3,21 +3,42 @@ import { useTheme } from '@mui/material/styles';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import Button from '@mui/material/Button';
-import { Theme, ColorModeContext } from '@/theme';
-import NeumorphismPannel from '@/components/NeumorphismPannel';
+import { Theme, ColorModeContext, getNeumorphismByThemeMode } from '@/theme';
 
 export default function ThemeModeSwitch() {
   const theme = useTheme();
   const colorMode = useContext(ColorModeContext);
   const iconSx: any = (theme: Theme) => ({ color: theme.palette.common.white });
-  return (
+  const text = `${theme.palette.mode} mode`;
+  return theme.palette.mode === 'dark' ? (
     <Button
       className="absolute top-4 right-6"
       variant="contained"
       onClick={colorMode.toggleColorMode}
-      endIcon={theme.palette.mode === 'dark' ? <Brightness7Icon sx={iconSx} /> : <Brightness4Icon sx={iconSx} />}
+      endIcon={<Brightness4Icon sx={iconSx} />}
+      sx={[(theme: Theme) => {
+        const neumorphismParams = { shadowDistance: '6px', shadowBlur: '6px' };
+        const neuObj = getNeumorphismByThemeMode({ theme, neumorphismParams });
+        return {
+          '&.MuiButton-root': {
+            ...neuObj.pressed,
+            '&:hover': {
+              ...neuObj.flat
+            }
+          }
+        };
+      }] as any}
     >
-      {theme.palette.mode} mode
+      {text}
+    </Button>
+  ) : (
+    <Button
+      className="absolute top-4 right-6"
+      variant="contained"
+      onClick={colorMode.toggleColorMode}
+      startIcon={<Brightness7Icon sx={iconSx} />}
+    >
+      {text}
     </Button>
   );
 }
